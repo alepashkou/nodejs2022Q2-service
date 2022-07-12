2925a62ea8986a67ede92bbc5bf3be40d7c7523d
Alexander Pashkou <ale.pashkou@gmail.com>
2022-07-12 08:32:03 +0300

    feat: add services to modules

--- src/album/album.module.ts
+++ src/album/album.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AlbumController } from './album.controller';
+import { Service } from './.service';
+import { AlbumService } from './album.service';
 
 @Module({
-  controllers: [AlbumController]
+  controllers: [AlbumController],
+  providers: [Service, AlbumService],
 })
 export class AlbumModule {}
--- src/shared/db/db.ts
+++
@@ -1,6 +0,0 @@
-export const db = {
-  Users: [],
-  Artists: [],
-  Albums: [],
-  Tracks: [],
-};
